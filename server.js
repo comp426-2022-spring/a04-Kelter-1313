3c55c433a7d7bf35678d32c01c46ddfdb6c1072f
Albert "Christian" DeMarco <Kelter-1313>
2022-04-28 14:04:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get("/app", (req, res, next) => {
   next()
 })
 
-if(args["debug"] == true){
+if(args["debug"] == "true"){
   /* must create endpoints
 
   --debug	If set to true, creates endlpoints /app/log/access/ which returns
@@ -93,7 +93,7 @@ if(args["debug"] == true){
     throw new Error('Error test successful.')
   })
 
-  app.get("/app/log/access", (req, res, next) => {
+  app.get("/app/log/access/", (req, res, next) => {
     try{
       const stmt = db.prepare('SELECT * FROM accesslog').all()
       res.status(200).json(stmt)
